5b73ff830b60f320555a9fdc7d8e6f9e535d1a68
unknown <imohgenius@yahoo.com>
2019-11-09 19:44:18 +0100

    list of products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,7 @@ products: Product[];
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getProducts(1).subscribe(response =>
+    this._productService.getProducts().subscribe(response =>
       this.products = response);
   }
 
